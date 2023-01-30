f6b3a41ab2470f5d55cf464c327dfb609489637f
nathan ks <nathanks0210@gmail.com>
2023-01-30 17:04:22 -0300

    criacao ambiente

---
+++ src/main/java/nks/rest_universidades/ApiRestUniversidadesApplication.java
@@ -0,0 +1,13 @@
+package nks.rest_universidades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestUniversidadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestUniversidadesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nks/rest_universidades/ApiRestUniversidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package nks.rest_universidades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestUniversidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
