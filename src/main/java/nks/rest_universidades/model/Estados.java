9b167a50d26ea41c5bd7c59ae48c65df06af8c5e
nathan ks <nathanks0210@gmail.com>
2023-01-31 14:52:18 -0300

    classes estado e universidades e sua relação

---
+++ src/main/java/nks/rest_universidades/model/Estados.java
@@ -0,0 +1,54 @@
+package nks.rest_universidades.model;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class Estados {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "sigla", nullable = false)
+    private String sigla;
+
+    @ManyToMany()
+    private List<Universidade> universidade;
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public String getSigla() {
+        return this.sigla;
+    }
+
+
+}
---
+++ src/main/java/nks/rest_universidades/model/Universidade.java
@@ -0,0 +1,62 @@
+package nks.rest_universidades.model;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity(name = "universidades")
+public class Universidade {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "sigla", nullable = false)
+    private String sigla;
+
+    @Column(name = "site", nullable = true)
+    private String site;
+
+    @ManyToMany(mappedBy = "universidade")
+    private List<Estados> estados;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getSigla() {
+        return sigla;
+    }
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+    public String getSite() {
+        return site;
+    }
+    public void setSite(String site) {
+        this.site = site;
+    }
+    public List<Estados> getEstados() {
+        return estados;
+    }
+    public void setEstados(List<Estados> estados) {
+        this.estados = estados;
+    }
+
+}
