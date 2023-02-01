0b9fdbe8a856f1dba495135319100e1ad5073d17
nathan ks <nathanks0210@gmail.com>
2023-01-31 21:58:28 -0300

    relacao campus univeridade

--- src/main/java/nks/rest_universidades/model/Campus.java
+++ src/main/java/nks/rest_universidades/model/Campus.java
@@ -7,7 +7,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 
 @Entity(name = "campus")
 public class Campus {
@@ -24,6 +26,10 @@ public class Campus {
 
     @ManyToMany
     private List<Curso> curso;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Universidade universidade;
 
     public int getId() {
         return id;
