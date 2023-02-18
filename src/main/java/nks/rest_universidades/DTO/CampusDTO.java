716fd79cd06c243ee790e7ff3a12ca48c84661fc
nathan ks <nathanks0210@gmail.com>
2023-02-18 19:33:25 -0300

    busca de campus

---
+++ src/main/java/nks/rest_universidades/DTO/CampusDTO.java
@@ -0,0 +1,9 @@
+package nks.rest_universidades.DTO;
+
+public interface CampusDTO {
+    
+    String getLocalização();
+    String getSite();
+    String getUniversidade();
+
+}
---
+++ src/main/java/nks/rest_universidades/controller/CampusController.java
@@ -0,0 +1,29 @@
+package nks.rest_universidades.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import nks.rest_universidades.DTO.CampusDTO;
+import nks.rest_universidades.model.Campus;
+import nks.rest_universidades.repository.campusRepository;
+
+@RestController
+public class CampusController {
+    
+    @Autowired
+    private campusRepository repository;
+
+    @GetMapping(path = "/universidades/campus/listar")
+    public List<Campus> listar(){
+        return repository.findAll();
+    }
+
+    @GetMapping(path = "/universidades/campus/universidade/{sigla}")
+    public List<CampusDTO> campusPorUniversidade(@PathVariable("sigla") String sigla){
+        return repository.findCampusByUniversidade(sigla);
+    }
+}
