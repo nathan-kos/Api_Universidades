4995f414db7bfd721515c3354102100ad57a08e0
nathan ks <nathanks0210@gmail.com>
2023-02-18 18:54:53 -0300

    organizando contollers

--- src/main/java/nks/rest_universidades/controller/EstadosController.java
+++ src/main/java/nks/rest_universidades/controller/EstadosController.java
@@ -4,31 +4,20 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import nks.rest_universidades.model.Estados;
-import nks.rest_universidades.model.Universidade;
 //import nks.rest_universidades.model.Universidade;
 import nks.rest_universidades.repository.EstadosRepository;
-import nks.rest_universidades.repository.UniversidadeRepository;
 
 @RestController
 public class EstadosController {
     
     @Autowired
     private EstadosRepository eRepository;
-    @Autowired
-    private UniversidadeRepository uRepository;
 
     @GetMapping(path = "/universidades/estados/listar")
     public List<Estados> ConsultarEstados(){
         return eRepository.findAll();
     }
-
-    @GetMapping(path = "/universidades/estados/{sigla}")
-    public List<Universidade> ConsultarUniversidadesPorEstado(@PathVariable String sigla){
-        return uRepository.findUniversidadesByEstado(sigla);
-    }
-
 }
---
+++ src/main/java/nks/rest_universidades/controller/UniversidadeController.java
@@ -0,0 +1,30 @@
+package nks.rest_universidades.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import nks.rest_universidades.DTO.UniEstDTO;
+import nks.rest_universidades.model.Universidade;
+import nks.rest_universidades.repository.UniversidadeRepository;
+
+@RestController
+public class UniversidadeController {
+    
+    @Autowired
+    private UniversidadeRepository repository;
+
+    @GetMapping(path = "/universidades/universidade/listar")
+    public List<Universidade> findAll() {
+        return repository.findAll();
+    }
+
+
+    @GetMapping(path = "/universidades/universidade/estado/{sigla}")
+    public List<UniEstDTO> ConsultarUniversidadesPorEstado(@PathVariable String sigla){
+        return repository.findUniversidadesByEstado(sigla);
+    }
+}
