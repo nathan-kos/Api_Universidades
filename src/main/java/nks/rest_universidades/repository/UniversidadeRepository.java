1e6e91c7a3c468036de2422025e4e54b49880cb7
nathan ks <nathanks0210@gmail.com>
2023-02-18 18:56:17 -0300

    concertando erro

--- src/main/java/nks/rest_universidades/repository/UniversidadeRepository.java
+++ src/main/java/nks/rest_universidades/repository/UniversidadeRepository.java
@@ -5,10 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import nks.rest_universidades.DTO.UniEstDTO;
 import nks.rest_universidades.model.Universidade;
 
 public interface UniversidadeRepository extends JpaRepository<Universidade, Integer> {
     
     @Query(value = "select universidades.nome as universidade, estados.nome as estados from universidades join estados_universidades on estados_universidades.universidade_id = universidades.id join estados on estados_universidades.estados_id = estados.id where estados.sigla = ?1", nativeQuery = true)
-    public List<Universidade> findUniversidadesByEstado(String sigla);
+    public List<UniEstDTO> findUniversidadesByEstado(String sigla);
 }
